6403ebc040cc1cbf0bf330197ac5a4504719cc18
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2025-10-30 17:26:36 +0800

    change

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,4 +1,19 @@
 
-document.getElementById("datetime").textContent = new Date().getFullYear();
+function updateLastModified() {
+  const lastModified = document.lastModified;
+  document.getElementById('last-modified').textContent = lastModified;
+}
+
+function updateYear() {
+  const year = new Date().getFullYear();
+  document.getElementById('current-year').textContent = year;
+}
+
+// Run functions after the page loads
+document.addEventListener('DOMContentLoaded', () => {
+  updateLastModified();
+  updateYear();
+});
+
 
 
