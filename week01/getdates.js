c9aefbc73a41a4a0cf543da8b7194dffd1606076
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2025-10-30 10:36:24 +0800

    test1

--- week01/getdates.js
+++ week01/getdates.js
@@ -1,14 +1,15 @@
+function updateLastModified() {
+  const lastModified = document.lastModified;
+  document.getElementById('last-modified').textContent = lastModified;
+}
 
-        function updateLastModified() {
-            const lastModified = document.lastModified;
-            document.getElementById('last-modified').textContent = lastModified;
-        }
+function updateYear() {
+  const year = new Date().getFullYear();
+  document.getElementById('current-year').textContent = year;
+}
 
-    
-        function updateYear() {
-            const year = new Date().getFullYear();
-            document.getElementById('current-year').textContent = year;
-        }
-
-        updateLastModified();
-        updateYear()
+// Run functions after the page loads
+document.addEventListener('DOMContentLoaded', () => {
+  updateLastModified();
+  updateYear();
+});
