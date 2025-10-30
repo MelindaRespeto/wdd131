9b7f1c0f4fbeb6a04d2abcb6543a07e1fcee1a22
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2025-10-30 12:23:04 +0800

    change

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,15 @@
+function updateLastModified() {
+  const lastModified = document.lastModified;
+  document.getElementById('last-modified').textContent = lastModified;
+}
+
+function updateYear() {
+  const year = new Date().getFullYear();
+  document.getElementById('current-year').textContent = year;
+}
+
+// Run functions after the page loads
+document.addEventListener('DOMContentLoaded', () => {
+  updateLastModified();
+  updateYear();
+});
