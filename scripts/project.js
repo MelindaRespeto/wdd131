10f776c06deec090c8bb6f8710d49d4014ed632d
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2025-12-07 07:45:30 +0800

    add

--- scripts/project.js
+++ scripts/project.js
@@ -1,6 +1,10 @@
 
 
-
+ document.addEventListener("DOMContentLoaded", function() {
+     document.getElementById("currentYear").textContent = new Date().getFullYear();
+     document.getElementById("lastModified").textContent = new Date(document.lastModified).toLocaleString();
+            });
+        
 function getCurrentDate() {
     let currentDate = new Date();
     let year = currentDate.getFullYear();
